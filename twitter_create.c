3d3ccdaed3256664f9478d8dcba226f118a19570
ciaranscan6 <cpt.scanlon@gmail.com>
2022-04-21 16:59:31 +0100

    please work

---
+++ twitter_create.c
@@ -0,0 +1,76 @@
+//
+// Created by jacob on 14/04/2022.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include "twitter_create.h"
+
+void create_twitter_system(twitter * twitter_system)
+{
+    //to create 100 blank users with there user name taken from the standard input
+
+    user users[MAX_USERS];
+    tweet newsFeed[MAX_TWEETS];
+    int number_of_tweets = 0;
+
+    //loop to create the users
+    for(int i = 0; i < MAX_USERS; i++)
+    {
+        printf("Input the username of the next OR Press <Enter> to terminate:\n");
+        fgets(users[i].username, USR_LENGHT, stdin);
+
+        //code to take a backslash n from the string
+
+        if(users[i].username[strlen(users[i].username)-1] == '\n')
+        {
+            users[i].username[strlen(users[i].username)-1] = '\0';
+        }
+
+        //condition for if the user presses enter
+
+        if(strcasecmp("", users[i].username) == 0)
+        {
+            return;
+        }
+
+        //put the user in the actual twitter structure under users
+
+        strcpy(twitter_system->users[i].username, users[i].username);
+
+        //increment the amount of actual users
+
+        twitter_system->num_users++;
+
+
+        //initialise also the followers to 0 and the number of following to 0 too
+
+        twitter_system->users[i].num_followers = 0;
+        twitter_system->users[i].num_following = 0;
+    }
+}
+//this function should take 2 users a and b then make a follow b
+
+void follow(user * a, user * b)
+{
+    //condition as to whether a and b are the same user
+    if(strcasecmp(a->username, b->username) == 0)
+    {
+        printf("A user cannot follow themselves");
+        return;
+    }
+
+    //copy b's username into the array that a is following
+    strcpy(a->following[a->num_following], b->username);
+
+    //increment the number a is following by 1
+    a->num_following++;
+
+    //update b to have a as a follower
+    //put a's name in b's followers list
+
+    strcpy(b->followers[b->num_followers], a->username);
+
+    //increment the number of followers b has by one
+    b->num_followers++;
+}
